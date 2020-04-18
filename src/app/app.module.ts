7165c5431b32f17d0d704355cef9e9338039a2b0
Eduardo Perez <eddoros@gmail.com>
2020-04-17 19:13:32 -0600

    Revert "new grid drag and drop - WIP"
    This reverts commit edca362956fbd8e4256bacc1f7e8c6c53674c3b6.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,27 +6,18 @@ import { BreederComponent } from './breeder/breeder.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DragDropModule } from '@angular/cdk/drag-drop';
-import { DisplayComponent } from './display/display.component';
-import { DragSourceRenderer } from './display/drag-source-renderer.component';
-
-// ag-grid
-import { AgGridModule } from 'ag-grid-angular';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    BreederComponent,
-    DisplayComponent,
-    DragSourceRenderer
+    BreederComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    DragDropModule,
-    AgGridModule.withComponents([DragSourceRenderer])
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
