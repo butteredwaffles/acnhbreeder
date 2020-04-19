d95f33963a181d6b3a2f9865cf4ef1eee3abe48f
Eduardo Perez <eddoros@gmail.com>
2020-04-19 00:29:28 -0600

    material grid and loads of bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 
 @NgModule({
@@ -19,6 +20,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    MatGridListModule,
     ReactiveFormsModule,
     BrowserAnimationsModule,
     MatCardModule,
