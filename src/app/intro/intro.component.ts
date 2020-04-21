5f94bccab9e8484bfc3cfe3703988f5f58aa8e09
qwaffles <nobodywinds@gmail.com>
2020-04-21 13:29:34 -0400

    try to make some accomodations for diff screens

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./intro.component.scss']
 })
 export class IntroComponent implements OnInit {
+  screenWidth = window.screen.width;
 
   constructor() { }
 
